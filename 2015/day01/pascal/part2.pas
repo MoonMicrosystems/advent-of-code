27d11fb8b9055354f1a9ec4e584559f3c4f19961
Moon Microsystems <182438103+MoonMicrosystems@users.noreply.github.com>
2024-10-14 22:10:41 +0300

    feat(2015,day01): solve part I + II (Free Pascal)
    Signed-off-by: Moon Microsystems <182438103+MoonMicrosystems@users.noreply.github.com>

---
+++ 2015/day01/pascal/part2.pas
@@ -0,0 +1,78 @@
+program Part1;
+
+{$mode objfpc}{$H+}
+
+uses
+  SysUtils;
+
+function ReadFileContentsAsString(const FilePath: string): string;
+var
+  FileData: TextFile;
+  Line: string;
+begin
+  Result := '';
+  if FileExists(FilePath) then
+  begin
+    AssignFile(FileData, FilePath);
+    Reset(FileData);
+    while not EOF(FileData) do
+    begin
+      ReadLn(FileData, Line);
+      Result := Result + Line;
+    end;
+    CloseFile(FileData);
+  end
+end;
+
+function Part2(const FileContents: string): integer;
+var
+  i: integer;
+  CurrChar: string;
+  Res: Integer;
+begin
+  Res := 0;
+  for i := 1 to Length(FileContents) do
+  begin
+    CurrChar := FileContents[i];
+    if CurrChar = '(' then
+      Inc(Res)
+    else if CurrChar = ')' then
+      Dec(Res);
+
+    if Res = -1 then
+      Exit(i);
+  end;
+end;
+
+var
+  FileName: String;
+  FileContents: string;
+  Expected, Actual: Integer;
+  ExpectedResults: array[5..9] of Integer = (1, 3, 1, 1, 1);
+  AllTestsPassed: Boolean = True;
+  I: Integer;
+begin
+  for i := 5 to 9 do
+  begin
+    FileName := Format('../inputs/sample.%d', [i]);
+    FileContents := ReadFileContentsAsString(FileName);
+    Expected := ExpectedResults[i];
+    Actual := Part2(FileContents);
+    if Expected = Actual then
+      Writeln('Sample ', I, ' is correct!')
+    else
+    begin
+      AllTestsPassed := False;
+      Writeln('Sample ', I, ' is incorrect!');
+      Writeln('Expected: ', Expected);
+      Writeln('Actual:   ', Actual);
+    end;
+  end;
+
+  if AllTestsPassed then
+  begin
+    FileContents := ReadFileContentsAsString('../inputs/input');
+    Actual := Part2(FileContents);
+    Writeln('Result for part 2: ', Actual);
+  end;
+end.
